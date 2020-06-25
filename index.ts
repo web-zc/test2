e978b30e13d2c9344c453bd613cd2aacd58cf540
zcmax <443662456@qq.com>
2020-06-25 23:11:52 +0800

    fix: 怎么回事
    *人不会输

--- index.ts
+++ index.ts
@@ -1,17 +1,27 @@
-class  B{
-// 不22
+class B {
+  // 不22
 }
 // 工厂类
-class Zc{
-  static create(){
+class Zc {
+  static create() {
     return new B()
   }
 }
 
 // 不直接返回实例，右工厂类返回
-class NewB{
-static  create(){
-   return Zc.create()
+class NewB {
+  static create() {
+    return Zc.create()
   }
 }
 NewB.create()
+
+function zc() {
+  setInterval(() => {
+
+  })
+}
+function ab() {
+  return zc()
+}
+ab()
